987cb616880ec680fc7ea776091119d545bd3d15
Ambuj007 <ambujjnv@gmail.com>
2019-07-21 16:04:31 +0530

    added test and travis config

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from shop.models import Notice
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Notice.objects.create(Notice="lion")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        
+        lion = Notice.objects.get(Notice="lion")
+        self.assertIsNotNone(lion)
+        # cat = Animal.objects.get(name="cat")
+        # self.assertEqual(lion.speak(), 'The lion says "roar"')
+        # self.assertEqual(cat.speak(), 'The cat says "meow"')
