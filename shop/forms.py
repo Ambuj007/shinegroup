25f51009b446b30b6b62446b69e862fda1effb18
Ambuj007 <ambujjnv@gmail.com>
2019-07-24 03:06:37 +0530

    Followed naming convention and some small tweeks"

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .Lists import PRODUCT_CHOICES, SPEAKER, STORAGE_SPEC, SPEAKER_TYPE, STORAGE, CAMERA_SPEC, BRAND, SMALL_STORAGE, KB_MOUSE, UPS, USER, ANTIVIRUS, PRINTER,COMP_PERIPHERALS, DVR_SMPS, CAMERA, CCTV
+from .Lists import PRODUCT_CHOICES, SPEAKER, STORAGE_SPEC, SPEAKER_TYPE, STORAGE, CAMERA_SPEC, BRAND, SMALL_STORAGE, KB_MOUSE, UPS, USER, ANTIVIRUS, PRINTER, COMP_PERIPHERALS, DVR_SMPS, CAMERA, CCTV
 from django.contrib.auth import authenticate
 
 
@@ -8,33 +8,51 @@ class ContactForm(forms.Form):
     subject = forms.CharField(required=False, max_length=100)
     sender = forms.EmailField(required=False)
     phone = forms.CharField(required=False, max_length=13)
-    message = forms.CharField(required=False, max_length=200, widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
+    message = forms.CharField(required=False, max_length=200, widget=forms.Textarea(
+        attrs={'rows': 4, 'cols': 40}))
 
 
-class ProductView(forms.Form):
-    Item            = forms.CharField(label='It Is', required=False, widget=forms.Select(attrs = {'class' : 'input-sm'},choices=sorted(PRODUCT_CHOICES, reverse=False)))
-    brand           = forms.CharField(required=False, widget=forms.Select(choices=sorted(BRAND, reverse=False)))
-    typep            = forms.CharField(max_length=120, label='Type',  required=False)
-    SPEAKER         = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(SPEAKER, reverse=False)))
-    STORAGE         = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(STORAGE, reverse=False)))
-    CCTV            = forms.CharField(label='Item', required=False, widget=forms.Select(choices=sorted(CCTV, reverse=False)))
-    CAMERA          = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(CAMERA, reverse=False)))
-    DVR_SMPS        = forms.CharField(label='Type', required=False, widget=forms.Select(choices=DVR_SMPS))
-    COMP_PERIPHERALS= forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(COMP_PERIPHERALS, reverse=False)))
-    KB_MOUSE       = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=sorted(KB_MOUSE, reverse=False)))
-    UPS             = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(UPS, reverse=False)))
-    ANTIVIRUS       = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(ANTIVIRUS, reverse=False)))
-    PRINTER         = forms.CharField(label='Type', required=False, widget=forms.Select(choices=sorted(PRINTER, reverse=False)))
-    specificationp   = forms.CharField(max_length=120, label='Specification',  required=False)
-    USER            = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=USER))
-    CAMERA_SPEC     = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=sorted(CAMERA_SPEC, reverse=False)))
-    STORAGE_SPEC    = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=STORAGE_SPEC))
-    SMALL_STORAGE    = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=SMALL_STORAGE))
-    SPEAKER_TYPE    = forms.CharField(label='Specification', required=False, widget=forms.Select(choices=sorted(SPEAKER_TYPE, reverse=False)))
-    model           = forms.CharField(max_length=120, required=False)
+class StockForm(forms.Form):
+    item_class = forms.CharField(label='It Is', required=False, widget=forms.Select(
+        attrs={'class': 'input-sm'}, choices=sorted(PRODUCT_CHOICES, reverse=False)))
+    brand = forms.CharField(required=False, widget=forms.Select(
+        choices=sorted(BRAND, reverse=False)))
+    item_type = forms.CharField(max_length=120, label='Type',  required=False)
+    speaker = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(SPEAKER, reverse=False)))
+    storage = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(STORAGE, reverse=False)))
+    cctv = forms.CharField(label='Item', required=False, widget=forms.Select(
+        choices=sorted(CCTV, reverse=False)))
+    camera = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(CAMERA, reverse=False)))
+    dvr_smps = forms.CharField(
+        label='Type', required=False, widget=forms.Select(choices=DVR_SMPS))
+    comp_peripherals = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(COMP_PERIPHERALS, reverse=False)))
+    kb_mouse = forms.CharField(label='Specification', required=False, widget=forms.Select(
+        choices=sorted(KB_MOUSE, reverse=False)))
+    ups = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(UPS, reverse=False)))
+    antivirus = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(ANTIVIRUS, reverse=False)))
+    printer = forms.CharField(label='Type', required=False, widget=forms.Select(
+        choices=sorted(PRINTER, reverse=False)))
+    item_specification = forms.CharField(
+        max_length=120, label='Specification',  required=False)
+    user = forms.CharField(label='Specification',
+                           required=False, widget=forms.Select(choices=USER))
+    camera_spec = forms.CharField(label='Specification', required=False, widget=forms.Select(
+        choices=sorted(CAMERA_SPEC, reverse=False)))
+    storage_spec = forms.CharField(
+        label='Specification', required=False, widget=forms.Select(choices=STORAGE_SPEC))
+    small_storage = forms.CharField(
+        label='Specification', required=False, widget=forms.Select(choices=SMALL_STORAGE))
+    speaker_type = forms.CharField(label='Specification', required=False, widget=forms.Select(
+        choices=sorted(SPEAKER_TYPE, reverse=False)))
+    model_no = forms.CharField(max_length=120, required=False)
     purchase_price = forms.DecimalField(max_digits=10, decimal_places=2)
-    selling_price   = forms.DecimalField(max_digits=10, decimal_places=2)
-
+    selling_price = forms.DecimalField(max_digits=10, decimal_places=2)
 
     def clean(self, *args, **kwargs):
         selling_price = self.cleaned_data.get("selling_price")
@@ -43,13 +61,11 @@ class ProductView(forms.Form):
             raise forms.ValidationError("Ismein Tera Ghata")
         return super(ProductView, self).clean(*args, **kwargs)
 
-class ProductView1(forms.Form):
-    product= forms.CharField(label='What are you looking for?', required=False, widget=forms.Select(choices=PRODUCT_CHOICES))
-
 
 class LoginForm(forms.Form):
-    username     = forms.CharField(label='User', required=True)
-    password     = forms.CharField(label='Password', required=True, widget=forms.PasswordInput())
+    username = forms.CharField(label='User', required=True)
+    password = forms.CharField(
+        label='Password', required=True, widget=forms.PasswordInput())
 
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get("username")
@@ -68,11 +84,10 @@ class LoginForm(forms.Form):
         return super(LoginForm, self).clean(*args, **kwargs)
 
 
-
 class NoticeForm(forms.Form):
-    Notice = forms.CharField(required=False, max_length=100)
+    notice = forms.CharField(required=False, max_length=100)
 
 
-class Demand(forms.Form):
-    Item     = forms.CharField(required=True, max_length=100)
-    Quantity = forms.IntegerField(required=True, min_value=1)
+class DemandForm(forms.Form):
+    item = forms.CharField(required=True, max_length=100)
+    quantity = forms.IntegerField(required=True, min_value=1)
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import ContactForm, ProductView, LoginForm, Demand
+from .forms import ContactForm, StockForm, LoginForm, DemandForm
 from django.core.mail import send_mail
 from django.conf import settings
-from .models import Stock, Notice, DemandModel
+from .models import Stock, Notice, Demand
 from django.template.loader import get_template
 from django.views.generic.edit import DeleteView, UpdateView
 from django.views.generic import CreateView, FormView, ListView
@@ -28,13 +28,13 @@ import xlwt
 class DeleteItem(DeleteView):
     model = Stock
     template_name = 'shop/delete_Item.html'
-    success_url = '../InventoryDetail'
+    success_url = '../inventory_detail'
 
 
 #Item Detail List View
 
 class ItemListView(ListView):
-    template_name='shop/Item_detail.html'
+    template_name='shop/item_detail.html'
     model = Stock
     context_object_name = 'object'
 
@@ -42,29 +42,30 @@ class ItemListView(ListView):
 class NoticeView(CreateView):
     template_name='shop/notice.html'
     model = Notice
-    fields = ['Notice']
+    fields = ['notice']
     success_url = '/'
 
 
-def DemandView(request):
-    form = Demand()
+def demand_view(request):
+    form = DemandForm()
     if request.POST:
-        form = Demand(request.POST)
+        form = DemandForm(request.POST)
         if form.is_valid():
-            Item = form.cleaned_data['Item'] 
-            Quantity = form.cleaned_data['Quantity']  
+            item = form.cleaned_data['item'] 
+            quantity = form.cleaned_data['quantity']  
             context = {
-            'Item' : Item,
-            'Quantity' : Quantity
+            'item' : item,
+            'quantity' : quantity
             }
-            DemandModel.objects.create(**context)
+            Demand.objects.create(**context)
             messages.success(request, 'Note kar liya hai, bagal me check kar lo')
-            form = Demand()
-    demands_all = DemandModel.objects.all()
+            form = DemandForm()
+    demands_all = Demand.objects.all()
+    print(demands_all[2])
     return render(request, 'shop/demand.html', {'form':form, 'object':demands_all})
 
 
-def DemandDownloadView(request):
+def demand_download_view(request):
     response = HttpResponse(content_type = 'application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename="On_Demand.xls"'
     
@@ -84,7 +85,7 @@ def DemandDownloadView(request):
     font_style = xlwt.XFStyle()
 
 
-    object = DemandModel.objects.all().values_list('Item', 'Quantity')
+    object = Demand.objects.all().values_list('item', 'quantity')
     
     for row in object:
         row_num += 1
@@ -103,10 +104,10 @@ def DemandDownloadView(request):
 
 #Update View
 class ItemUpdateView(UpdateView):
-    template_name='shop/Item_Update.html'
+    template_name='shop/item_update.html'
     model = Stock
-    fields = ['category','brand','item','typep','specificationp','Model_No','purchase_price','selling_price']
-    success_url = '../InventoryDetail'
+    fields = ['category','brand','item','item_type','item_specification','model_no','purchase_price','selling_price']
+    success_url = '../inventory_detail'
 
 
 def index(request):
@@ -120,7 +121,7 @@ def index(request):
 def services(request):
     return render(request, 'shop/services.html')
 
-def ThankYou(request):
+def thank_you(request):
     return render(request, 'shop/thank_you.html')
 
 def about(request):
@@ -187,7 +188,7 @@ class LoginView(FormView):
         return super(LoginView, self).form_invalid(form)
 
 
-def Logoutview(request):
+def logout_view(request):
     logout(request)
     return redirect("/")
 
@@ -212,11 +213,11 @@ def contact(request):
             'reciever' : reciever
         }
 
-        contactmessage = get_template('shop/email.txt').render(context)
+        contact_message = get_template('shop/email.txt').render(context)
         #print(contact_message)
         send_mail(
             subject,
-            contactmessage,
+            contact_message,
             sender,
             reciever,
             fail_silently = True
@@ -226,63 +227,62 @@ def contact(request):
     return render(request, 'shop/contact.html',  {'form':form})
 
 
-def inventorymanagement(request):
+def inventory_management(request):
     new_model = {}
-    form = ProductView(request.POST or None)
+    form = StockForm(request.POST or None)
     if form.is_valid():
-        category_m = form.cleaned_data['Item']
+        category_m = form.cleaned_data['item_class']
         brand_m = form.cleaned_data['brand']
-        model_no_m = form.cleaned_data['model']
+        model_no_m = form.cleaned_data['model_no']
         purchase_price_m = form.cleaned_data['purchase_price']
         selling_price_m = form.cleaned_data['selling_price']
         item_m =""
         type_m= ""
         specification_m =  ""
 
-        if form.cleaned_data['Item'] == "CCTV":
-            item_m = form.cleaned_data['CCTV']
-            if form.cleaned_data['CCTV'] == "Camera":
-                type_m = form.cleaned_data['CAMERA']
-                specification_m = form.cleaned_data['CAMERA_SPEC']
-            elif form.cleaned_data['CCTV'] == "DVR" or form.cleaned_data['CCTV'] == "SMPS":
-                type_m = form.cleaned_data['DVR_SMPS']
+        if form.cleaned_data['item_class'] == "CCTV":
+            item_m = form.cleaned_data['cctv']
+            if form.cleaned_data['cctv'] == "Camera":
+                type_m = form.cleaned_data['camera']
+                specification_m = form.cleaned_data['camera_spec']
+            elif form.cleaned_data['cctv'] == "DVR" or form.cleaned_data['cctv'] == "SMPS":
+                type_m = form.cleaned_data['dvr_smps']
 
 
-        elif form.cleaned_data['Item'] == "Computer Peripherals":
-            item_m = form.cleaned_data['COMP_PERIPHERALS']
-            if form.cleaned_data['COMP_PERIPHERALS'] == "Keyboarad" or form.cleaned_data['COMP_PERIPHERALS'] == "Mouse":
+        elif form.cleaned_data['item_class'] == "Computer Peripherals":
+            item_m = form.cleaned_data['comp_peripherals']
+            if form.cleaned_data['comp_peripherals'] == "Keyboarad" or form.cleaned_data['comp_peripherals'] == "Mouse":
                 specification_m = form.cleaned_data['KB_MOUSE']
-            elif form.cleaned_data['COMP_PERIPHERALS'] == "RAM" or form.cleaned_data['COMP_PERIPHERALS'] == "GC":
-                specification_m = form.cleaned_data['SMALL_STORAGE']
+            elif form.cleaned_data['comp_peripherals'] == "RAM" or form.cleaned_data['comp_peripherals'] == "GC":
+                specification_m = form.cleaned_data['small_storage']
 
 
-        elif form.cleaned_data['Item'] == "Storage":
-            type_m = form.cleaned_data['STORAGE']
-            specification_m = form.cleaned_data['STORAGE_SPEC']
+        elif form.cleaned_data['item_class'] == "Storage":
+            type_m = form.cleaned_data['storage']
+            specification_m = form.cleaned_data['storage_spec']
 
 
-        elif form.cleaned_data['Item'] == "Printer":
-            type_m = form.cleaned_data['PRINTER']
+        elif form.cleaned_data['item_class'] == "Printer":
+            type_m = form.cleaned_data['printer']
 
 
-        elif form.cleaned_data['Item'] == "Ups":
-            type_m = form.cleaned_data['STORAGE']
-            specification_m = form.cleaned_data['STORAGE_SPEC']
+        elif form.cleaned_data['item_class'] == "Ups":
+            type_m = form.cleaned_data['ups']
+            
+
+        elif form.cleaned_data['item_class'] == "Speakers":
+            type_m = form.cleaned_data['speaker']
+            specification_m = form.cleaned_data['speaker_type']
 
 
-        elif form.cleaned_data['Item'] == "Speakers":
-            type_m = form.cleaned_data['SPEAKER']
-            specification_m = form.cleaned_data['SPEAKER_TYPE']
-
-
-        elif form.cleaned_data['Item'] == "Antivirus":
-            type_m = form.cleaned_data['ANTIVIRUS']
-            specification_m = form.cleaned_data['USER']
+        elif form.cleaned_data['item_class'] == "Antivirus":
+            type_m = form.cleaned_data['antivirus']
+            specification_m = form.cleaned_data['user']
 
 
         else:
-            type_m = form.cleaned_data['typep']
-            specification_m = form.cleaned_data['specificationp']
+            type_m = form.cleaned_data['item_type']
+            specification_m = form.cleaned_data['item_specification']
 
 
 
@@ -290,9 +290,9 @@ def inventorymanagement(request):
                 'category' : category_m,
                 'brand' : brand_m,
                 'item' : item_m,
-                'typep' : type_m,
-                'specificationp' : specification_m,
-                'Model_No' : model_no_m,
+                'item_type' : type_m,
+                'item_specification' : specification_m,
+                'model_no' : model_no_m,
                 'purchase_price' : purchase_price_m,
                 'selling_price' :selling_price_m
 
@@ -300,7 +300,7 @@ def inventorymanagement(request):
 
         #print(form.cleaned_data['Item'])
         Stock.objects.create(**new_model)
-        form = ProductView()
+        form = StockForm()
 
-    return render(request, 'shop/inventorymanagement.html', {'form':form})
+    return render(request, 'shop/inventory_management.html', {'form':form})
 
